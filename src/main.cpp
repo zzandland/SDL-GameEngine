0fce74004f029992b5a2e37c89391f6cc9dda878
Si Yong Kim <zzandland@gmail.com>
2021-09-18 17:46:26 -0400

    limit the fps to 60

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include "Game.hpp"
 
+#define FPS 60
+#define FRAME_DELAY FPS / 60
+
 Game *game;
 int main(int argc, const char **argv) {
   game = new Game();
@@ -7,10 +10,20 @@ int main(int argc, const char **argv) {
   game->init("SDL Game Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
              800, 600, false);
 
+  unsigned frameStart, frameTime;
+
   while (game->running()) {
+    frameStart = SDL_GetTicks();
+
     game->handleEvents();
     game->update();
     game->render();
+
+    frameTime = SDL_GetTicks() - frameStart;
+
+    if (FRAME_DELAY > frameTime) {
+      SDL_Delay(FRAME_DELAY - frameTime);
+    }
   }
 
   game->clean();
