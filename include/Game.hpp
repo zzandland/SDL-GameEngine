e62ee59d2ba0a4bd8834c8cc60d53449fd621250
Si Yong Kim <zzandland@gmail.com>
2021-09-18 08:50:21 -0400

    Initial Game class update

---
+++ include/Game.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+
+#include "SDL.h"
+
+class Game {
+ public:
+  Game();
+  ~Game();
+
+  void init(const char *title, int xpos, int ypos, int width, int height,
+            bool fullscreen);
+  void handleEvents();
+  void update();
+  void render();
+  void clean();
+
+  bool running() { return isRunning; };
+
+ private:
+  bool isRunning;
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
-#include <iostream>
+#include "Game.hpp"
 
-int main() {
-    std::cout << "Hello Easy C++ project!" << std::endl;
+Game *game;
+int main(int argc, const char **argv) {
+  game = new Game();
+
+  game->init("SDL Game Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+             800, 600, false);
+
+  while (game->running()) {
+    game->handleEvents();
+    game->update();
+    game->render();
+  }
+
+  game->clean();
+
+  return EXIT_SUCCESS;
 }
